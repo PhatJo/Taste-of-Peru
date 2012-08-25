cdb374cbe0b40880fa9559d9fc46207db9571529
Pietro Rea <pietrorea@gmail.com>
2012-08-25 01:40:53 -0400

    Initial commit

---
+++ src/iPhone/Supporting Files/TPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPAppDelegate.h
+//  Taste of Peru
+//
+//  Created by Pietro Rea on 8/22/12.
+//  Copyright (c) 2012 Pietro Rea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ src/iPhone/Supporting Files/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Taste of Peru
+//
+//  Created by Pietro Rea on 8/22/12.
+//  Copyright (c) 2012 Pietro Rea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPAppDelegate class]));
+    }
+}
