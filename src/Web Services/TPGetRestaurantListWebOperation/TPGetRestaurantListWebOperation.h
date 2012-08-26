99f95606b18aef77164ab913a8b5e61213635ef1
Pietro Rea <pietrorea@gmail.com>
2012-08-26 19:04:15 -0400

    Added AFNetworking framework to project

---
+++ src/Web Services/TPGetRestaurantListWebOperation/TPGetRestaurantListWebOperation.h
@@ -0,0 +1,13 @@
+//
+//  TPGetRestaurantListWebOperation.h
+//  Taste of Peru
+//
+//  Created by Pietro Rea on 8/26/12.
+//  Copyright (c) 2012 Pietro Rea. All rights reserved.
+//
+
+#import "TPBaseWebServiceOperation.h"
+
+@interface TPGetRestaurantListWebOperation : TPBaseWebServiceOperation
+
+@end
---
+++ src/Web Services/TPGetRestaurantListWebOperation/TPGetRestaurantListWebOperation.m
@@ -0,0 +1,13 @@
+//
+//  TPGetRestaurantListWebOperation.m
+//  Taste of Peru
+//
+//  Created by Pietro Rea on 8/26/12.
+//  Copyright (c) 2012 Pietro Rea. All rights reserved.
+//
+
+#import "TPGetRestaurantListWebOperation.h"
+
+@implementation TPGetRestaurantListWebOperation
+
+@end
